a66e0d97c383c6798fe72ae435e3531b481281e6
Walter Latimer, Jr <walter@walterlatimer.com>
2014-07-10 17:22:33 -0400

    Split headers/body

--- server.rb
+++ server.rb
@@ -1,11 +1,44 @@
 require 'sinatra'
 
 class WinViagraFreeSingles < Sinatra::Base
+
 	get '/' do
 		erb :index
 	end
 
 	post '/' do
+		@email = ParsedEmail.new(params[:file][:tempfile])
 		erb :parsed
+	end
+
+end
+
+class ParsedEmail
+	attr_reader :headers,
+	            :body
+	
+	def initialize(raw_email)
+		raw_headers, raw_body = split_headers_and_body(raw_email)
+		@headers = create_array_of_headers(raw_headers)
+	end
+
+	private
+
+	def split_headers_and_body(raw_email)
+		raw_email.read.split("\n\n", 2)
+	end
+
+	def create_array_of_headers(raw_headers)
+		headers = []
+		raw_headers.each_line do |line|
+
+			next if line =~ /^\s+$/ # Skip line if it's just whitespace
+			line.rstrip!
+
+			# Add line to the last element if it begins with whitespace,
+			# Otherwise, make it a new element
+			line =~ /^\s/ ? headers[-1] += line : headers << line
+		end
+		headers
 	end
 end
