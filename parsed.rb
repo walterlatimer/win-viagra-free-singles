2a93ac7dad0d339a8ac9dd59317ae8ced381aa96
Walter Latimer, Jr <walter@walterlatimer.com>
2014-07-11 01:20:59 -0400

    Added layout.erb, tackled some edge cases

--- parsed.rb
+++ parsed.rb
@@ -6,34 +6,41 @@ class ParsedEmail
 		@headers, @body = parse(raw_email)
 	end
 
-
-	def received() self.headers["Received"].to_s end
+	# Update these guys
 
 	def date() self.headers["Date"] end
+	def message_id() self.headers["Message-ID"] rescue "No Message ID" end
 
-	def message_id() self.headers["Message-ID"] end
 
 	# Method calls for sanity in views
-	def subject() self.headers["Subject"] end
+	def subject() self.headers["Subject"] || "[NO SUBJECT]" end
 	def content_type() self.headers["Content-Type"] end
 	def mime_version() self.headers["MIME-Version"] end
 
 
 	# Method calls listing email addresses as comma separated string
-	def from() addresses_in("From").join(", ") end
-	def to() addresses_in("To").join(", ") end
-	def cc() addresses_in("Cc").join(", ") end
-	def bcc() addresses_in("Bcc").join(", ") end
-	def delivered_to() addresses_in("Delivered-To").join(", ") end
-	def return_path() addresses_in("Return-Path").join(", ") end
+	def from() addresses_in("From").join(", ") rescue nil end
+	def to() addresses_in("To").join(", ") rescue nil end
+	def cc() addresses_in("Cc").join(", ") rescue nil end
+	def bcc() addresses_in("Bcc").join(", ") rescue nil end
+	def delivered_to() addresses_in("Delivered-To").join(", ") rescue nil end
+	def return_path() addresses_in("Return-Path").join(", ") rescue nil end
 
+
+	# Should always return an array, so make it an array if it's a string
+	def received()
+		received = self.headers["Received"]
+		received.class == String ? [received] : received
+	end
+
+	def real_sender()
+		self.received.last
+	end
 
 	# Returns an array of email addresses found in a given field and removes duplicates
 	def addresses_in field
-		self.headers[field].scan(/[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+/).flatten.uniq rescue []
+		self.headers[field].scan(/[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+/).flatten.uniq rescue nil
 	end
-
-
 
 
 
@@ -73,6 +80,7 @@ class ParsedEmail
 
 
 	def create_headers_hash headers_array
+		# Received may have multiple values, as array
 		headers = {}
 
 		# Store values as hash, but don't include duplicate values
